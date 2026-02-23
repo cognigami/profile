3820766bf521f89769ce4c934e1a2412eebe7428
Ricardo Olenewa <cognigami@proton.me>
2026-02-23 16:47:12 -0500

    Path work. 3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ export default defineConfig({
   site: 'https://YOUR-USERNAME.github.io',
   // base: '/YOUR-REPO-NAME', // uncomment if not using root domain
   build: {
-    assets: '_assets'
+    assets: 'assets'
   }
 });
